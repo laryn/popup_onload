33389b8e558b9dc60a5cf9b6eda26ecfb396b342
Dmitriy Novikov <dimmu.neivan@gmail.com>
2013-03-16 22:55:06 +0200

    Initial commit.

---
+++ popupad.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Javascript to display a popup after a delay.
+ */
+
+(function ($) {
+
+Drupal.behaviors.initPopupAd = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+    
+    $('body', context).once('popupad', function() {
+      var popupad_settings = settings.popupad;
+    
+      setTimeout(function() {
+        $.colorbox(popupad_settings);
+      }, popupad_settings.delay);
+    });
+  }
+}
+
+})(jQuery);
---
+++ popupad_statistics/popupad_statistics.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Attach clicks/views statistics JS to colorbox.
+ */
+
+(function ($) {
+
+Drupal.behaviors.attachPopupAdStatistics = {
+  
+  attach: function (context, settings) {
+	  if (!$.isFunction($.colorbox)) {
+        return;
+      }
+  
+	  function PopupAdStatisticsLogAction(atype) {
+	    var popupad_settings = settings.popupad;
+
+	    $.ajax({
+	      async: false,
+	      dataType: 'json',
+	      data: {
+	        atype: atype,
+	        popup_id: popupad_settings.popup_id
+	      },
+	      type: 'POST',
+	      url: '/popupad_stats_log'
+	    });
+	  }
+	  
+	  $(document).bind('cbox_complete', function() {
+		  PopupAdStatisticsLogAction('view');
+
+	  	$('#colorbox.popupad a, #colorbox.popupad area').click(function (event) {
+	      PopupAdStatisticsLogAction('click');
+	      return true;
+	    });
+	    $('#colorbox.popupad form').submit(function (event) {
+	      PopupAdStatisticsLogAction('click');
+	      return true;
+	    });
+	  });
+  }
+}
+  
+})(jQuery);
