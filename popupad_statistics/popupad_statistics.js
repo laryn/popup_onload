988075ce2127997d1179cb3af8ce7671654976fc
Dmitriy Novikov <dimmu.neivan@gmail.com>
2013-03-19 14:46:39 +0200

    Minor code style fixes.

--- popupad_statistics/popupad_statistics.js
+++ popupad_statistics/popupad_statistics.js
@@ -6,40 +6,40 @@
 (function ($) {
 
 Drupal.behaviors.attachPopupAdStatistics = {
-  
+
   attach: function (context, settings) {
-	  if (!$.isFunction($.colorbox)) {
+    if (!$.isFunction($.colorbox)) {
         return;
       }
-  
-	  function PopupAdStatisticsLogAction(atype) {
-	    var popupad_settings = settings.popupad;
 
-	    $.ajax({
-	      async: false,
-	      dataType: 'json',
-	      data: {
-	        atype: atype,
-	        popup_id: popupad_settings.popup_id
-	      },
-	      type: 'POST',
-	      url: '/popupad_stats_log'
-	    });
-	  }
-	  
-	  $(document).bind('cbox_complete', function() {
-		  PopupAdStatisticsLogAction('view');
+    function PopupAdStatisticsLogAction(atype) {
+      var popupad_settings = settings.popupad;
 
-	  	$('#colorbox.popupad a, #colorbox.popupad area').click(function (event) {
-	      PopupAdStatisticsLogAction('click');
-	      return true;
-	    });
-	    $('#colorbox.popupad form').submit(function (event) {
-	      PopupAdStatisticsLogAction('click');
-	      return true;
-	    });
-	  });
+      $.ajax({
+        async: false,
+        dataType: 'json',
+        data: {
+          atype: atype,
+          popup_id: popupad_settings.popup_id
+        },
+        type: 'POST',
+        url: '/popupad_stats_log'
+      });
+    }
+
+    $(document).bind('cbox_complete', function() {
+      PopupAdStatisticsLogAction('view');
+
+      $('#colorbox.popupad a, #colorbox.popupad area').click(function (event) {
+        PopupAdStatisticsLogAction('click');
+        return true;
+      });
+      $('#colorbox.popupad form').submit(function (event) {
+        PopupAdStatisticsLogAction('click');
+        return true;
+      });
+    });
   }
 }
-  
+
 })(jQuery);
